43f8a8da6ab1c58e3c166d11e0388129238b1ac0
Yixin Sun <sun955967@gmail.com>
2017-03-24 00:02:45 -0400

    test push

--- submit.php
+++ submit.php
@@ -8,7 +8,11 @@
   <?php
 
 /*connection to mySQL and select database*/
+ $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+ if(mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ $database = mysqli_select_db($connection, DB_DATABASE);
 
+ /* Ensure User table exists */
 
 <html lang="en">
 <head>
